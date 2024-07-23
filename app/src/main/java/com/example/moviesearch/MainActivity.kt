f7b95caee1aca3c0d844b5a9dcd231f6abf3aecd
Raghav <raaghav.gupta17@gmail.com>
2024-07-23 11:49:19 -0700

    test

--- app/src/main/java/com/example/moviesearch/MainActivity.kt
+++ app/src/main/java/com/example/moviesearch/MainActivity.kt
@@ -56,44 +56,44 @@ class MainActivity : AppCompatActivity() {
 
 
     private fun fetchMovieData() {
-        val apiKey = BuildConfig.API_KEY
-
-        lifecycleScope.launchWhenCreated {
-            try{
-                val response = RetrofitInstance.api.searchMovie(apiKey,
-                binding.idMovieSearchEt.text.toString(),
-                    1)
-
-                if(response.isSuccessful){
-                    Log.d(TAG, "fetchMovieData: ${response.body()?.results?.get(0).toString()}")
-
-                    val movieDetailsFrag = MovieDetailsFragment()
-                    val fragmentTransaction = supportFragmentManager.beginTransaction()
-
-                    val mBundle = Bundle()
-
-                    //Putting all the data that we need to send to fragment
-                    mBundle.putString("title", response.body()?.results?.get(0)?.title)
-                    mBundle.putString("overview", response.body()?.results?.get(0)?.overview)
-                    mBundle.putString("date", response.body()?.results?.get(0)?.release_date)
-                    mBundle.putString("poster", response.body()?.results?.get(0)?.poster_path)
-                    mBundle.putInt("vote_count", response.body()?.results?.get(0)?.vote_count!!)
-
-                    movieDetailsFrag.arguments = mBundle
-                    fragmentTransaction.replace(R.id.id_fragment_container, movieDetailsFrag).commit()
-                    //supportFragmentManager.popBackStack("FragA", FragmentManager.POP_BACK_STACK_INCLUSIVE)
-
-                }else{
-                    Log.d(TAG, "fetchMovieData: ${response.body().toString()}")
-                    Toast.makeText(this@MainActivity, "fail", Toast.LENGTH_SHORT).show()
-                }
-
-            }catch (e: Exception){
-                Log.d(TAG, "fetchMovieData: ${e.localizedMessage.toString()}")
-                Toast.makeText(this@MainActivity, "abc", Toast.LENGTH_SHORT).show()
-            }
-
-        }
+//        val apiKey = BuildConfig.API_KEY
+//
+//        lifecycleScope.launchWhenCreated {
+//            try{
+//                val response = RetrofitInstance.api.searchMovie(apiKey,
+//                binding.idMovieSearchEt.text.toString(),
+//                    1)
+//
+//                if(response.isSuccessful){
+//                    Log.d(TAG, "fetchMovieData: ${response.body()?.results?.get(0).toString()}")
+//
+//                    val movieDetailsFrag = MovieDetailsFragment()
+//                    val fragmentTransaction = supportFragmentManager.beginTransaction()
+//
+//                    val mBundle = Bundle()
+//
+//                    //Putting all the data that we need to send to fragment
+//                    mBundle.putString("title", response.body()?.results?.get(0)?.title)
+//                    mBundle.putString("overview", response.body()?.results?.get(0)?.overview)
+//                    mBundle.putString("date", response.body()?.results?.get(0)?.release_date)
+//                    mBundle.putString("poster", response.body()?.results?.get(0)?.poster_path)
+//                    mBundle.putInt("vote_count", response.body()?.results?.get(0)?.vote_count!!)
+//
+//                    movieDetailsFrag.arguments = mBundle
+//                    fragmentTransaction.replace(R.id.id_fragment_container, movieDetailsFrag).commit()
+//                    //supportFragmentManager.popBackStack("FragA", FragmentManager.POP_BACK_STACK_INCLUSIVE)
+//
+//                }else{
+//                    Log.d(TAG, "fetchMovieData: ${response.body().toString()}")
+//                    Toast.makeText(this@MainActivity, "fail", Toast.LENGTH_SHORT).show()
+//                }
+//
+//            }catch (e: Exception){
+//                Log.d(TAG, "fetchMovieData: ${e.localizedMessage.toString()}")
+//                Toast.makeText(this@MainActivity, "abc", Toast.LENGTH_SHORT).show()
+//            }
+//
+//        }
     }
 
 
